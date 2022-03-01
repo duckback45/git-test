e227e90c7c664bfaf87fab482e96af4058136890
duckback45 <duckback45@gmail.com>
2022-03-02 02:20:31 +0900

    create main dev2

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,6 +1,6 @@
 public class main {
     public static void main(String[] args) {
-        System.out.println("hihihi");
         System.out.println("dev");
+        System.out.println("dev2");
     }
 }
